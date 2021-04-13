88119661240fdf9964c5bcda858da4f887856aeb
tayson13 <ab.naser01@gmail.com>
2021-04-13 12:36:42 +0600

    second commit

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','App\Http\Controllers\HomeController@home')->name('homepage');
 
+Route::get('/','App\Http\Controllers\HomeController@index')->name('index');
+
